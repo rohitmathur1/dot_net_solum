5a855827efa6f1628e7d91926cf4775c1080f060
rohitmathur1 <rohitmathur1@users.noreply.github.com>
2014-05-13 20:58:25 +0530

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldSample
+{
+	public class HelloWorld
+	{
+		public static void Main()
+		{
+			System.Console.WriteLine("Hello World. First program");
+		}
+	}
+}
