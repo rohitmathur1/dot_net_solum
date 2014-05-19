58d7e9011632ffcd10031bb056b0bbee0c3f7fcb
rohitmathur1 <rohitmathur1@users.noreply.github.com>
2014-05-20 02:27:31 +0530

    Update Program.cs

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("This is my first program! Yay!");
+        }
+    }
+}
